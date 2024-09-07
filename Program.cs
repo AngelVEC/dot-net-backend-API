c3c576576cfb58b266861f79025ae316f49831e5
angelvec <angelmystic15@gmail.com>
2024-09-07 16:36:50 +1200

    fixing the problem that database not accessaible

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using dot_net_backend_api.Data;
 using dot_net_backend_api.Query;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// make connection to the database
+builder.Services.AddDbContext<ApiDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("postgressConnection")));
+
+//Graphql
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>();
--- Query/Query.cs
+++ Query/Query.cs
@@ -5,11 +5,14 @@ using dot_net_backend_api.Data;
 namespace dot_net_backend_api.Query
 {
     public class Query{
+        
+        //for User object
         public async Task<List<User>> AllUsersAsync([Service] ApiDbContext dbContext)
         {
             return await dbContext.Users.ToListAsync();
         }
 
+        // for food object
         public async Task<List<Food>> AllfoodsAsync([Service] ApiDbContext dbContext)
         {
             return await dbContext.Foods.ToListAsync();
