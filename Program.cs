45866119f8f5f2aeaa59c5259cf08a9caaf399e2
angelvec <angelmystic15@gmail.com>
2024-09-13 16:18:42 +1200

    fixing cors, so ReactJS can access the page

--- Program.cs
+++ Program.cs
@@ -4,10 +4,23 @@ using dot_net_backend_api.Query;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // make connection to the database
 builder.Services.AddDbContext<ApiDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("postgressConnection")));
+
+//Cors for localhost react
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                            policy.AllowAnyHeader();
+                            policy.AllowAnyMethod();
+                            policy.WithOrigins("http://localhost:3000");
+                      });
+});
 
 //Graphql
 builder.Services
@@ -19,5 +32,8 @@ var app = builder.Build();
 
 //To make /graphql accessable
 app.MapGraphQL();
+
+//To allow cors origin
+app.UseCors(MyAllowSpecificOrigins);
 
 app.Run();
